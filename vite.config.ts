a986acdcf904dcb13d0664fccdfbba8ec1ac0a3e
Harshitha-Yallamati <harshithay2203@gmail.com>
2026-07-16 22:07:41 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Merchant360/', // Required for GitHub Pages deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
